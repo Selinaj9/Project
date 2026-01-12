213369694d38133a69034785f85ff13831d179bd
BTHS <sample@bths.edu>
2026-01-12 11:03:31 -0500

    Game details Status: In progress

---
+++ src/Encryption.java
@@ -0,0 +1,14 @@
+public class Encryption {
+    private String[] cipherTypes = {"Caesar cipher"};
+
+    public Encryption() {
+
+    }
+
+    public String[] getCipherTypes {
+        return cipherTypes;
+    }
+    public void cipher(String c) {
+
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,8 @@
+public class Game {
+
+    public Game() { }
+
+    public void play() {
+        System.out.print("Available ciphers: ");
+    }
+}
