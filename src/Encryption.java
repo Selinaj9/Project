a0b1681dc82c50e2903d983112040e1f7f88757b
BTHS <sample@bths.edu>
2026-01-14 10:54:48 -0500

    Game details: 3 ciphers created and implemented
    Overall Status: In progress

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,14 +1,65 @@
 public class Encryption {
-    private String[] cipherTypes = {"Caesar cipher"};
+    private String[] cipherTypes;
+    private String alphabet;
 
     public Encryption() {
-
+        cipherTypes = new String[]{"Caesar Cipher", "Atbash Cipher", "Affine Cipher"};
+        alphabet = "abcdefghijklmnopqrstuvwxyz";
     }
 
-    public String[] getCipherTypes {
+    public String[] getCipherTypes() {
         return cipherTypes;
     }
-    public void cipher(String c) {
 
+    public String caesarCipher(String word, int shift) {
+        String encrypt = "";
+        String crypt = alphabet.substring(shift) + alphabet.substring(0,shift);
+        for (int i = 0; i < word.length(); i++) {
+            String letter = word.substring(i, i + 1);
+            if (!letter.matches("[a-zA-z]")) {
+                encrypt += letter;
+            } else {
+                int idx = alphabet.indexOf(letter);
+                encrypt += crypt.substring(idx, idx + 1);
+            }
+        }
+        return encrypt;
+    }
+
+    public String atbashCipher(String word) {
+        String encrypt = "";
+        String crypt = "";
+        for (int i = alphabet.length() - 1; i >= 0; i--) {
+            crypt += alphabet.substring(i, i + 1);
+        }
+        for (int i = 0; i < word.length(); i ++) {
+            String letter = word.substring(i, i + 1);
+            if (!letter.matches("[a-zA-z]")) {
+                encrypt += letter;
+            } else {
+                int idx = alphabet.indexOf(letter);
+                encrypt += crypt.substring(idx, idx + 1);
+            }
+        }
+        return encrypt;
+    }
+
+    public String affineCipher(String word) {
+        String encrypt = "";
+        String crypt = "";
+        for (int i = 0; i < alphabet.length(); i++) {
+            int idx = (5 * i + 8) % 26;
+            crypt += alphabet.substring(idx, idx + 1);
+        }
+        for (int i = 0; i < word.length(); i ++) {
+            String letter = word.substring(i, i + 1);
+            if (!letter.matches("[a-zA-z]")) {
+                encrypt += letter;
+            } else {
+                int idx = alphabet.indexOf(letter);
+                encrypt += crypt.substring(idx, idx + 1);
+            }
+        }
+        return encrypt;
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,43 @@
-public class Game {
+import java.util.Arrays;
+import java.util.Scanner;
 
-    public Game() { }
+public class Game {
+    private Encryption encryption;
+    private Scanner scan = new Scanner(System.in);
+    private String word;
+    private String cipher;
+    private boolean stop;
+
+    public Game() {
+        encryption = new Encryption();
+        word = "";
+        cipher = "";
+        stop = false;
+    }
 
     public void play() {
-        System.out.print("Available ciphers: ");
+        while (!stop) {
+            System.out.print("Enter the word/sentence you would like to encrypt (lowercase only): ");
+            word = scan.nextLine();
+            System.out.print("Choose a cipher: " + Arrays.toString(encryption.getCipherTypes()) + " ");
+            cipher = scan.nextLine();
+            if (cipher.equals("Caesar Cipher")) {
+                System.out.print("Enter number of shift: ");
+                int shift = scan.nextInt();
+                String encrypt = encryption.caesarCipher(word, shift);
+                System.out.println("The encrypted word is: " + encrypt);
+            } else if (cipher.equals("Atbash Cipher")) {
+                String encrypt = encryption.atbashCipher(word);
+                System.out.println("The encrypted word is: " + encrypt);
+            } else if (cipher.equals("Affine Cipher")) {
+                String encrypt = encryption.affineCipher(word);
+                System.out.println("The encrypted word is: " + encrypt);
+            }
+            System.out.println("Would you like to encrypt another word/sentence?");
+            String ans = scan.nextLine();
+            if (ans.equals("no")) {
+                stop = true;
+            }
+        }
     }
 }
